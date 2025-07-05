67d767bf446fbbdf4d7d34ece966334281c5a439
Aman Chaurasia <112266282+Aman-code-master@users.noreply.github.com>
2025-07-05 12:31:44 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "B. Hyper Text MarkUp Language",
+        options: [
+            "A. Hyper Type Multi Language",
+            "B. Hyper Text MarkUp Language",
+            "C. Hyper Text Multiple Language",
+            "D. Home Text Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does CSS stand for?",
+        answer: "A. Cascading Style Sheet",
+        options: [
+            "A. Cascading Style Sheet",
+            "B. Cute Style Sheet",
+            "C. Computer Style Sheet",
+            "D. Code Style Sheet"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does PHP stand for?",
+        answer: "A. HyperText Preprocessor",
+        options: [
+            "A. HyperText Preprocessor",
+            "B. HomeText Programming",
+            "C. HyperText Preprogramming",
+            "D. Programming Hypertext Preprocessor"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does SQL stand for?",
+        answer: "D. Structured Query Language",
+        options: [
+            "A. Strength Query Language",
+            "B. StyleSheet Query Language",
+            "C. Science Query Language",
+            "D. Structured Query Language"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What does XML stand for?",
+        answer: "D. Extensible Markup Language",
+        options: [
+            "A. Excellent Multiple Language",
+            "B. Explore Multiple Language",
+            "C. Extra Markup Language",
+            "D. Extensible Markup Language"
+        ]
+    }
+];
